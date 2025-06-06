b037c48f13d96e542f49efb790ec0118630e0506
D3vlin <alexisdq16@gmail.com>
2025-06-06 05:52:47 -0500

    Initial commit

---
+++ src/test/java/com/d3vlin13/pizzeria/PizzeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d3vlin13.pizzeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
